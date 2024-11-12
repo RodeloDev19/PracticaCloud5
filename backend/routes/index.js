8555297934f3f6ef98e3d6c5839da4c1040fd9fd
engel <angel.ochoa@iteso.mx>
2024-11-12 00:56:15 -0600

    actividad 3

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/message', (req, res) => {
-  res.json({ message: 'Hola desde backend!' });
+  res.json({ message: 'Hola desde el backend!' });
 });
 
 module.exports = router;
